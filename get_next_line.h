201f6b5a7c306fe6bf83fd0fe605ba51cf8df2dd
Luiz Fernando <lfluiz.lf@gmail.com>
2021-08-04 21:38:56 -0300

    Review Header file with norminette

--- get_next_line.h
+++ get_next_line.h
@@ -5,20 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: luizfern <lfluiz.lf@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/18 01:53:14 by luizfern          #+#    #+#             */
-/*   Updated: 2021/08/04 19:28:36 by luizfern         ###   ########.fr       */
+/*   Created: 2021/08/04 21:36:21 by luizfern          #+#    #+#             */
+/*   Updated: 2021/08/04 21:37:19 by luizfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H 
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include	<fcntl.h>
-# include	<stdio.h>
-# include	<unistd.h>
-# include	<stdlib.h>
-# include	<stddef.h>
-
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
@@ -26,7 +22,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
 char	*ft_strdup(const char *src);
-//char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_custom_strjoin(char *s1, char *s2);
 int		ft_custom_strchr(char *str, char c);
+
 #endif
